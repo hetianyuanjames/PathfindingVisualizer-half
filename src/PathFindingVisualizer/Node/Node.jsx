d1c2ffd1a151a07ee93f188558730bc987e83cdb
hetianyuanjames <hty1016@ad.unc.edu>
2022-07-14 15:17:29 -0400

    updated

---
+++ src/PathFindingVisualizer/Node/Node.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import './Node.css';
+
+
+
+export default class PathFindingVisualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        const {isFinish, isStart, isVisited} = this.props;
+        const extraClassName = isFinish
+            ? 'node-finish'
+            : isStart
+            ? 'node-start' 
+            :isVisited
+            ? 'node-visited'
+            : '';
+
+        return <div className={`node ${extraClassName}`}></div>
+    }
+}
+
+export const DEFAULT_NODE = {
+    row: 0,
+    col: 0,
+}
